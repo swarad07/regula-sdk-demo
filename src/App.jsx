1135f4e2c6648ee3f912f53bed4bf5098abb6110
Swarad <swarad07@gmail.com>
2025-02-22 12:32:59 +0530

    Adding output on the page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import { defineComponents, DocumentReaderService } from '@regulaforensics/vp-frontend-document-components';
 import '@regulaforensics/vp-frontend-face-components';
-import { converBase64ToImage } from 'convert-base64-to-image';
 
 const buttonStyle = {
   padding: '10px 30px',
@@ -25,8 +24,12 @@ function App() {
   const faceCaptureElementRef = useRef(null);
   const faceLivenessElementRef = useRef(null);
   const [documentReaderResponse, setDocumentReaderResponse] = useState(null);
+  const [faceLivenessResponse, setFaceLivenessResponse] = useState(null);
   const [locale, setLocale] = useState('en');
 
+  /**
+   * Get locale options
+   */
   const getLocaleOptions = () => {
     const locales = ['en', 'es', 'zh-Hans', 'ja', 'ko'];
     return locales.map((locale) => (
@@ -92,8 +95,11 @@ function App() {
     const faceLivenessComponent = document.querySelector('.face-liveness-container');
     faceLivenessComponent.addEventListener('face-liveness', faceLivenessListener);
 
-  }, [locale, documentReaderResponse]);
+  }, [locale, documentReaderResponse, faceLivenessResponse]);
 
+  /**
+   * Face Capture Listener
+   */
   function faceCaptureListener(data) {
     console.log(data);
     console.log(data.detail.action)
@@ -109,14 +115,17 @@ function App() {
     }
   }
 
+  /**
+   * Face Liveness Listener
+   */
   function faceLivenessListener(data) {
-    console.log(data);
-    console.log(data.detail.action)
     if (data.detail.action === 'PROCESS_FINISHED') {
-      const response = data.detail.data.response;
-      const rawImage = 'data:image/jpeg;base64,' + response.images[0];
-      console.log(rawImage)
-      console.log('Face Liveness Process Finished with status ' + data.detail.data.status);
+      console.log(data.detail.data);
+      setFaceLivenessResponse(data.detail.data);
+      // const response = data.detail.data.response;
+      // const rawImage = 'data:image/jpeg;base64,' + response.images[0];
+      // console.log(rawImage)
+      // console.log('Face Liveness Process Finished with status ' + data.detail.data.status);
     }
 
     if (data.detail?.action === 'CLOSE') {
@@ -124,12 +133,41 @@ function App() {
     }
   }
 
-  const generateImg = (base64) => {
-    // const img = document.createElement('img');
-    return 'data:image/jpeg;base64,' + base64;
-    // return img;
+  const generateFaceLivenessResponseMarkup = (data) => {
+    if (!data) return;
+    const { code, estimatedAge, status, images } = data.response;
+    return (
+      <>
+        <div className="row">
+          <h4>Code</h4>
+          <p>{code}</p>
+        </div>
+        <div className="row">
+          <h4>Estimated Age</h4>
+          <p>{estimatedAge}</p>
+        </div>
+        <div className="row">
+          <h4>Status</h4>
+          <p>{status}</p>
+        </div>
+        <div className="row">
+          <h4>Image</h4>
+          <p><img src={generateImg(images[0])} alt="Face Liveness" /></p>
+        </div>
+      </>
+    );
   };
 
+  /**
+   * Generate complete image base64 string
+   */
+  const generateImg = (base64) => {
+    return 'data:image/jpeg;base64,' + base64;
+  };
+
+  /**
+   * Generate text fields markup
+   */
   const generateTextFields = (fields) => {
     if (!fields) return;
     const textFields = fields.map((field, index) => {
@@ -144,6 +182,9 @@ function App() {
     return textFields;
   };
 
+  /**
+   * Generate Image fields markup
+   */
   const generateImageFields = (fields) => {
     if (!fields) return;
     const imageFields = fields.map((field, index) => {
@@ -159,7 +200,6 @@ function App() {
 
     return imageFields;
   };
-
 
   function documentReaderListener(data) {
     console.log(data.detail.action);
@@ -187,7 +227,6 @@ function App() {
         button.style.display = 'block';
     }
   }
-
 
   window.RegulaDocumentSDK = new DocumentReaderService();
 
@@ -222,18 +261,14 @@ function App() {
     },
   };
 
-  defineComponents().then(() => window.RegulaDocumentSDK.initialize({ license: 'AAEAAESFb1zjjaK+vbgfIIMKIwS+TGmvhsrpUuTNCaVjY2smJc2gOCulmK6nbgHDdvGDpEqvmPgPsM25/I50yKCdx4jWcE25onpznjPPoBdSZ4chG1OJV2StGnbe6MG+N250ofonnCg7ATsRujSYUCqI6OjR8c/nNbKv+iC2ASWWos3Js8Q3Pubt4PX+shLquv6d0wB/Cev2TASe+DlxGtddgrCZl+lFaKCvMrmkK5nrui57QVZbYR4SaHBrN9eTnGPAZVejJukDsdOnPizrZVbmP7UlHtZL5GmylMk7hT5NgRgIjoMU1gGirknFsmU1kAZMIat1WIIgLxg+8O529w2twW/kAAAAAAAAEPMvAhWSzHtZK9PpKm0moMzQ9TXkWqkb3yTfqyAHy2jb56yiYz1QuR6bQo0PfMJ9AQM9h2wBsjlGg7ywf6sx8NagesbJk2Q38JAgMH5lnSd3nnDqXDjmRzdRQ5qHD58E1f4CEMi5qB09NXLswZ4GCyUjRDrPwLXrdaJdpyPw4xtzlZNlTctsUwSEfcHRTiDa6oDBF3Nu2Jh7ipBy1us59R3yhrZRsSyKI7+120CxPu3hBpmDpf2PLydOVqpx8J6ajl7k1xT5h90xZiY+VctAAww4riomV+6XmP4k1o4PWtas' }));
-
-  const updateLocale = (e) => {
-    setLocale(e.target.value);
-  };
+  defineComponents().then(() => window.RegulaDocumentSDK.initialize({ license: import.meta.env.VITE_REACT_APP_REGULA_LICENSE }));
 
   return (
     <>
       <div className="locale-options">
         <h2>Locale Options</h2>
           <label htmlFor="locale">Locale</label>
-          <select id="locale" onChange={(e) => updateLocale(e)}>
+          <select id="locale" onChange={(e) => setLocale(e.target.value)}>
             {getLocaleOptions()}
           </select>
       </div>
@@ -262,11 +297,18 @@ function App() {
         </div>
       </div>
       <div className="regula-response">
+        <h2>Response</h2>
         <div className="response-text-fields">
+          <h3>Document Reader Text fields</h3>
           {generateTextFields(documentReaderResponse?.response?.text?.fieldList)}
         </div>
         <div className="response-image-fields">
+          <h3>Document Reader Image fields</h3>
           {generateImageFields(documentReaderResponse?.response?.images?.fieldList)}
+        </div>
+        <div className="face-liveness-response">
+          <h3>Face Liveness Response</h3>
+          {generateFaceLivenessResponseMarkup(faceLivenessResponse)}
         </div>
       </div>
     </>
